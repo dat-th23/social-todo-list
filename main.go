af04dd34801d75107b99979066ddf1c04ed03fb3
dat-th23 <dat.th1230@gmail.com>
2024-09-27 09:23:04 +0700

    Chuẩn hóa Response, Paging và SQLModel

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"social-todo-list/common"
 	"strconv"
 	"strings"
-	"time"
 )
 
 type ItemStatus int
@@ -90,12 +90,10 @@ func (item *ItemStatus) UnmarshalJSON(data []byte) error {
 }
 
 type TodoItem struct {
-	Id          int         `json:"id" gorm:"column:id;"`
+	common.SQLModel
 	Title       string      `json:"title" gorm:"column:title;"`
 	Description string      `json:"description" gorm:"column:description;"`
 	Status      *ItemStatus `json:"status" gorm:"column:status;"`
-	CreatedAt   *time.Time  `json:"created_at" gorm:"column:created_at;"`
-	UpdatedAt   *time.Time  `json:"updated_at,omitempty" gorm:"column:updated_at;"`
 }
 
 func (TodoItem) TableName() string { return "todo_items" }
@@ -116,22 +114,6 @@ type TodoItemUpdate struct {
 }
 
 func (TodoItemUpdate) TableName() string { return TodoItem{}.TableName() }
-
-type Paging struct {
-	Page  int   `json:"page" form:"page"`
-	Limit int   `json:"limit" form:"limit"`
-	Total int64 `json:"total" form:"-"`
-}
-
-func (p *Paging) Process() {
-	if p.Page <= 0 {
-		p.Page = 1
-	}
-
-	if p.Limit <= 0 || p.Limit >= 100 {
-		p.Limit = 10
-	}
-}
 
 func main() {
 	dsn := os.Getenv("DB_CONN_STR")
@@ -189,15 +171,13 @@ func CreateItem(db *gorm.DB) func(c *gin.Context) {
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"data": data.Id,
-		})
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(data.Id))
 	}
 }
 
 func ListItem(db *gorm.DB) func(c *gin.Context) {
 	return func(c *gin.Context) {
-		var paging Paging
+		var paging common.Paging
 
 		if err := c.ShouldBind(&paging); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
@@ -228,10 +208,7 @@ func ListItem(db *gorm.DB) func(c *gin.Context) {
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"data":   result,
-			"paging": paging,
-		})
+		c.JSON(http.StatusOK, common.NewSuccessResponse(result, paging, nil))
 	}
 }
 
@@ -257,9 +234,7 @@ func GetItem(db *gorm.DB) func(c *gin.Context) {
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"data": data,
-		})
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(data))
 	}
 }
 
@@ -292,9 +267,7 @@ func UpdateItem(db *gorm.DB) func(c *gin.Context) {
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"data": true,
-		})
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(true))
 	}
 }
 
@@ -319,8 +292,6 @@ func DeleteItem(db *gorm.DB) func(c *gin.Context) {
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"data": true,
-		})
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(true))
 	}
 }
